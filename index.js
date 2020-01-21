0294694e3a1e43ddcc8879c31a3b88ef589b196d
janisrudis <rudis.janis@gmail.com>
2020-01-21 13:12:50 +0200

    Progress

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import {AppRegistry, YellowBox} from 'react-native';
+import React from 'react'
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {Catalog} from './src/views/Main/Shop/Catalog/Catalog'
+import {ApolloProvider} from '@apollo/react-hooks';
+import {client} from './src/apollo'
+import { name as appName } from './app.json';
 import storybook from './storybook';
-
 const IS_STORYBOOK = true;
-
 if (IS_STORYBOOK) {
   YellowBox.ignoreWarnings([
     'Warning: Async Storage has been extracted from react-native core',
   ]);
 }
-
-AppRegistry.registerComponent(appName, () => (IS_STORYBOOK ? storybook : App));
+AppRegistry.registerComponent(
+  appName,
+  () => () => <ApolloProvider client={client}>
+    <Catalog/>
+  </ApolloProvider>
+);
